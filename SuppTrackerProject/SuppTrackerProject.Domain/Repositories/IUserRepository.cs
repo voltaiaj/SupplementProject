db6018bd0b69bd98cb0c1678cc55d6e0a1164c33
voltaiaj <alexander.voltaire@gmail.com>
2015-08-20 12:16:12 -0500

    Added UserRepository and Interface

---
+++ SuppTrackerProject/SuppTrackerProject.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using SuppTrackerProject.Domain.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace SuppTrackerProject.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        User FindByEmail(string usernameEmail);
+        Task<User> FindByEmailAsync(string usernameEmail);
+        Task<User> FindByEmailAsync(CancellationToken cancellationToken, string usernameEmail);
+        User FindByUserName(string username);
+        Task<User> FindByUserNameAsync(string username);
+        Task<User> FindByUserNameAsync(CancellationToken cancellationToken, string username);
+        IRepository<User> Repo { get; }
+    }
+}
---
+++ SuppTrackerProject/SuppTrackerProject.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using SuppTrackerProject.Domain.Identity;
+using SuppTrackerProject.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuppTrackerProject.Infrastructure.Repositories
+{
+    internal class UserRepository : Repository<User>, IUserRepository
+    {
+        internal UserRepository(ApplicationDbContext context)
+            : base(context)
+        { 
+        }
+
+        //Property Provides access to base Repository<TEntity> members
+        public IRepository<User> Repo
+        {
+            get { return this; }
+        }
+
+        #region Universal UserMethods
+        public User FindByUserName(string username)
+        {
+            return Set.FirstOrDefault(x => x.UserName == username);
+        }
+
+        public Task<User> FindByUserNameAsync(string username)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public Task<User> FindByUserNameAsync(System.Threading.CancellationToken cancellationToken, string username)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == username, cancellationToken);
+        }
+
+        public User FindByEmail(string usernameEmail)
+        {
+            return Set.FirstOrDefault(x => x.UserName == usernameEmail);
+        }
+
+        public Task<User> FindByEmailAsync(string usernameEmail)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == usernameEmail);
+        }
+
+        public Task<User> FindByEmailAsync(System.Threading.CancellationToken cancellationToken, string usernameEmail)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == usernameEmail, cancellationToken);
+        }
+        #endregion
+    }
+}
