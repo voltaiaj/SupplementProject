ce23e90cb37273efb590bc5a5cca3a78253c6d82
voltaiaj <alexander.voltaire@gmail.com>
2015-08-22 21:52:47 -0500

    Adding ExternalLogin RoleRepository

---
+++ SuppTrackerProject/SuppTrackerProject.Infrastructure/Repositories/ExternalLoginRepository.cs
@@ -0,0 +1,33 @@
+﻿using SuppTrackerProject.Domain.Identity;
+using SuppTrackerProject.Domain.Repositories;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuppTrackerProject.Infrastructure.Repositories
+{
+    internal class ExternalLoginRepository : Repository<ExternalLogin>, IExternalLoginRepository
+    {
+        internal ExternalLoginRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public ExternalLogin GetByProviderAndKey(string loginProvider, string providerKey)
+        {
+            return Set.FirstOrDefault(x => x.LoginProvider == loginProvider && x.ProviderKey == providerKey);
+        }
+
+        public Task<ExternalLogin> GetByProviderAndKeyAsync(string loginProvider, string providerKey)
+        {
+            return Set.FirstOrDefaultAsync(x => x.LoginProvider == loginProvider && x.ProviderKey == providerKey);
+        }
+
+        public Task<ExternalLogin> GetByProviderAndKeyAsync(CancellationToken cancellationToken, string loginProvider, string providerKey)
+        {
+            return Set.FirstOrDefaultAsync(x => x.LoginProvider == loginProvider && x.ProviderKey == providerKey, cancellationToken);
+        }
+    }
+}
---
+++ SuppTrackerProject/SuppTrackerProject.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,37 @@
+﻿using SuppTrackerProject.Domain.Identity;
+using SuppTrackerProject.Domain.Repositories;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuppTrackerProject.Infrastructure.Repositories
+{
+    internal class RoleRepository : Repository<Role>, IRoleRepository
+    {
+        internal RoleRepository(ApplicationDbContext context)
+            : base(context)
+        { 
+        }
+
+        public IRepository<Role> Repo
+        {
+            get { return this; }
+        }
+
+        public Role FindByName(string roleName)
+        {
+            return Set.FirstOrDefault(x => x.Name == roleName);
+        }
+
+        public Task<Role> FindByNameAsync(string roleName)
+        {
+            return Set.FirstOrDefaultAsync(x => x.Name == roleName);
+        }
+
+        public Task<Role> FindByNameAsync(CancellationToken cancellationToken, string roleName)
+        {
+            return Set.FirstOrDefaultAsync(x => x.Name == roleName, cancellationToken);
+        }
+    }
+}
