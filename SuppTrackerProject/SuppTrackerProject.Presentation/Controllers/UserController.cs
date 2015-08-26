2859c5c53d358d26ec61d71548e9823db7ea7aae
voltaiaj <alexander.voltaire@gmail.com>
2015-08-25 23:56:57 -0500

    Progress Point 3

--- SuppTrackerProject/SuppTrackerProject.Presentation/Controllers/UserController.cs
+++ SuppTrackerProject/SuppTrackerProject.Presentation/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using SuppTrackerProject.Domain.Entities;
+using SuppTrackerProject.Presentation.Models;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace SuppTrackerProject.Presentation.Controllers
 {
@@ -14,10 +16,25 @@ namespace SuppTrackerProject.Presentation.Controllers
         private DataServices _dataServices;
         private AccountController _accountController = new AccountController();
 
-        [HttpGet]
-        public ActionResult Index()
-        {                        
-            
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            var result = _accountController.Login(model);
+            switch (result.Result)
+            {
+                case SignInStatus.Success:
+                    //return RedirectToLocal(returnUrl);
+                    TempData["loginModel"] = model;
+                    return RedirectToAction("Index", "User");
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
             //Guid userId = Guid.Parse(User.Identity.GetUserId());
             //IList<SupplementUser> result = _dataServices.SupplementUserService.GetSupplementUsersByUserId(userId);
             return View();
